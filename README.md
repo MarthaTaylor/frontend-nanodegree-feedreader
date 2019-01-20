7aca4b6e81c666859c9377ddcf660ecc2d177ac3
Martha Taylor <marthadigitalize@gmail.com>
2019-01-20 18:00:39 +0000

    updated to ES6 fixed New Feed Selection test

--- README.md
+++ README.md
@@ -1,17 +1,18 @@
 
-# RSS Feed Reader Testing with Jasmine 
+# Project 5 RSS Feed Reader Testing with Jasmine 
 
 ## Table of Contents
 
 - [Project Overview](#ProjectOverview)
 - [Why this Project?](#WhythisProject?)
 - [What I learned](#WhatIlearned)
+- [Built With](#BuiltWith)
 - [Installation](#Installation)
 - [External Code Used in this Project](#ExternalCodeUsedinthisProject)
 - [Credits](#credits)
 - [Contributing](#contributing)
 - [Acknowledgements](#Acknowledgements)
-- [License](#LICENSE)
+- [LICENSE](#LICENSE)
 
 
 # Project Overview
@@ -19,46 +20,60 @@
 The Udacity overview for this project states: In this project you are given a web-based application that reads RSS feeds. The original developer of this application clearly saw the value in testing, they've already included [Jasmine](http://jasmine.github.io/) and even started writing their first test suite! Unfortunately, they decided to move on to start their own company and we're now left with an application with an incomplete test suite. That's where you come in.
 
 
-## Why this Project?
+## Why this Project for TDD?
 
-The Udacity reasons states: Testing is an important part of the development process and many organizations practice a standard of development known as "test-driven development". This is when developers write tests first, before they ever start developing their application. All the tests initially fail and then they start writing application code to make these tests pass.
+The Udacity states: Testing is an important part of the development process and many organizations practice a standard of development known as "test-driven development". This is when developers write tests first, before they ever start developing their application. All the tests initially fail and then they start writing application code to make these tests pass.
 
 Whether you work in an organization that uses test-driven development or in an organization that uses tests to make sure future feature development doesn't break existing features, it's an important skill to have!
 
 
 ## What I learned
 
-To use Jasmine to write and run a number of tests against a pre-existing application successfully. These will test the underlying business logic of the application as well as the event handling and DOM manipulation. 
+To use Jasmine to write and run a number of tests against a pre-existing application successfully for behavior-driven development an extension of TDD that is more user focused. These will test the underlying business logic of the application as well as the event handling and DOM manipulation. I had fun using ES6 arrow functions syntax () => {} during this project.
+
+
+## Built and Tested With
+
+Languages:
+
+HTML
+CSS
+JavaScript
+
+Testing Framework:
+Jasmine 
+
 
 ## Installation
 
 Feel free to download the Feed Reader project to your machine.
 Jasmine is there already included and installed.
 
-1. Download the repository as a zip-file, extract it and open the index.html in your browser to run the Feed Reader Testing. 
-2. Clone the repository with Git Bash, first cd into the directory you wish to store your project in then run the command: 
-```$ git clone https://github.com/MarthaTaylor/frontend-nanodegree-feedreader``` and go to the folder, open the index.html in your browser to run the RSS Feed Reader Tests.
+*Download the repository as a zip-file, extract it and open the index.html in your browser to run the Feed Reader Testing. 
+*Clone the repository with Git Bash, first cd into the directory you wish to store your project in then run the command: ```$ git clone https://github.com/MarthaTaylor/frontend-nanodegree-feedreader``` and go to the folder, open the index.html in your browser to run the RSS Feed Reader Tests.
 
 ## External Code Used in this Project
 
 The resources at the following links helped me to get a better understanding of the arcade game project:
 
-1. The starter code for this project comes from the following [Udacity Frontend Feed Reader repository](https://github.com/udacity/frontend-nanodegree-feedreader). I forked and cloned it to my laptop. Creating the origin connection to the remote repository and pushed up my work to my remote repository here on GitHub.
+*The starter code for this project comes from the following (Udacity Frontend Feed Reader repository[here](https://github.com/udacity/frontend-nanodegree-feedreader)). I forked and cloned it to my laptop. Creating the origin connection to the remote repository and pushed up my work to my remote repository here on GitHub.
 
-2. The basic functions were taken from a mixture or [Mathew Cranford](www.mathewcranford.com) and [Mohamed Riaad's](https://www.youtube.com/watch?reload=9&v=Ut_L8YUImbw&index=6&list=PLpRo6-g-F-AOUsTQbKY1EYNpqXtxCT4a-&t=27s)).
+*The basic functions were taken from a mixture or (Mathew Cranford[here](www.mathewcranford.com)) and (Mohamed Riaad's[here](https://www.youtube.com/watch?reload=9&v=Ut_L8YUImbw&index=6&list=PLpRo6-g-F-AOUsTQbKY1EYNpqXtxCT4a-&t=27s)).
+
+*Some code from [Daniela Kuester project](https://github.com/DanielaKuester) thanks Daniella for helping me understand async calls with arrow functions
 
 
 ## Credits
 
 The Udacity course work, written tutorials by Mathew Cranford and youtube Study Jam by Mohamed Riaad, Jasmine Documentation and article from  helped me to get a good overview and understanding of the feed reader project and there approachs to TDD was insightful. 
 
-The [JavaScript Testing](https://eu.udacity.com/course/javascript-testing--ud549) course gives a solid introduction into testing with Jasmine.
+The (JavaScript Testing[here](https://eu.udacity.com/course/javascript-testing--ud549)) course gives a solid introduction into testing with Jasmine.
 
-The [Jasmine Documentation](https://jasmine.github.io/tutorials/your_first_suite) is excellent 
+The (Jasmine Documentation[here](https://jasmine.github.io/tutorials/your_first_suite)) is excellent 
 
-An overview of TDD and Jasmine's plus [Jasmines default matchers](https://medium.com/backticks-tildes/how-to-test-javascript-with-jasmine-framework-2e2b8dfa7a9e) written by Delores Diei which is very handy.
+An overview of TDD and Jasmine's plus (Jasmines default matchers[here] (https://medium.com/backticks-tildes/how-to-test-javascript-with-jasmine-framework-2e2b8dfa7a9e)) written by Delores Diei which is very handy.
 
-A comprehensive list on GitHub of [Jasmine's matchers](https://github.com/JamieMason/Jasmine-Matchers#jasmines-default-matchers)
+A comprehensive list on GitHub of (Jasmine's matchers[here](https://github.com/JamieMason/Jasmine-Matchers#jasmines-default-matchers))
 
 
 # Acknowledgements
@@ -67,18 +82,17 @@ Big thanks to all who support me during my projects at Udacity, at home and on t
 
 Especially my student group at Udacity/Google Scholarship Slack workspace and all of my study groups! 
 
-An article outlining an overview of TDD and Jasmine's plus [Jasmines default matchers](https://medium.com/backticks-tildes/how-to-test-javascript-with-jasmine-framework-2e2b8dfa7a9e) written by Delores Diei which is very handy, thanks Delores. The Jasmine documentation writers who ever you are you really made it easier to grasp the tool, thank you.
+An article outlining an overview of TDD and Jasmine's plus (Jasmines default matchers[here] (https://medium.com/backticks-tildes/how-to-test-javascript-with-jasmine-framework-2e2b8dfa7a9e)) written by Delores Diei which is very handy, thanks Delores. The Jasmine documentation writers who ever you are you really made it easier to grasp the tool, thank you.
 
-A special shout out to the talented Mathew Cranford for his awesome teaching written tutorials abilities, thank you!
+A special shout out to the talented Mathew Cranford for his awesome teaching written tutorials abilities and my code reviewer, thank you!
 
 # Contributing
 
-This repository is one of my projects as a Udacity student. I plan to update code too ES6 soon. Therefore, I most likely will not accept pull requests.
+This repository is one of my projects as a Udacity student. Therefore, I most likely will not accept pull requests.
 
 For details, check out CONTRIBUTING.md file in this project repository.
 
 # License
+(c) Martha Taylor 2018.
 
-(c) Martha Taylor 2018
-
-This repository is licensed under the [MIT license](https://choosealicense.com/licenses/mit/#). For further information see the LICENSE file in this project repository.
+This repository is licensed under the (MIT license[here](https://choosealicense.com/licenses/mit/#)). For further information see the LICENSE file in this project repository.
--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -13,7 +13,7 @@ $(function() {
     * a related set of tests. This suite is all about the RSS
     * feeds definitions, the allFeeds variable in our application.
     */
-    describe('RSS Feeds', function() {
+    describe('RSS Feeds', () => {
         /* This is our first test - it tests to make sure that the
          * allFeeds variable has been defined and that it is not
          * empty. Experiment with this before you get started on
@@ -21,7 +21,7 @@ $(function() {
          * allFeeds in app.js to be an empty array and refresh the
          * page?  
          */
-        it('rss feed is defined and a feed object/s have populated the allFeeds array', function() {
+        it('rss feed is defined and a feed object/s have populated the allFeeds array', () => {
             //ensure the allFeeds is defined
             expect(allFeeds).toBeDefined();
             //ensure the allFeeds is populated
@@ -29,11 +29,11 @@ $(function() {
         });
 
 
-        /* a test that loops through each feed
+        /* A test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
-         it('feed has url defined', function(){
+         it('feed has url defined', () => {
             for(let feed of allFeeds){
                 //console.log(feed); i used this to test if my loop worked            
                 expect(feed.url).toBeDefined();
@@ -42,21 +42,21 @@ $(function() {
                 //if when running the test you got expected 0 not to be 0 this means it expected the url length not to be empty 0
                 //can take out some url's and mess around with them in app.js to see if tests work + to get used to testing with Jasmine
                 expect(feed.url).toMatch("http");
-                //i just want to make sure the url cantains actual correct http address
+                //i just want to make sure the url cantains an actual http address
             }
          });
 
 
-        /* a test that loops through each feed
+        /* A test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
-         it('feed has a name defined', function(){
+         it('feed has a name defined', () => {
             for(let feed of allFeeds){
 
                 expect(feed.name).toBeDefined();
                 expect(feed.name.length).not.toBe(0);
-                // Check if the feed name is a string
+                //Check if the feed name is a string
                expect(feed.name).toEqual(jasmine.any(String));
             }
 
@@ -64,27 +64,28 @@ $(function() {
     });
 
 
-        /* a new test suite named "The menu" */
-    describe("The menu", function(){       
-         /* a test that ensures the menu element is
+        /*A new test suite named "The menu" */
+    describe("The menu", () => { 
+           //query the body element and assign body variable in outer scope now it can be accessed by both tests below
+            const body = document.querySelector('body')      
+         /* A test that ensures the menu element is
           * hidden by default. You'll have to analyze the HTML and
           * the CSS to determine how we're performing the
           * hiding/showing of the menu element.
           */
-         it('menu has an is hidden element by default', function(){
-            //query the body element then expect it’s class list to contain “menu-hidden” if it does it will be true.
-            const body = document.querySelector('body');
+         it('menu has an is hidden element by default', () => {
+            //body element queried above here expect it’s class list to contain “menu-hidden” if it does it will be true.
+            
             expect(body.classList.contains('menu-hidden')).toBe(true);
          });
 
-           /*a test that ensures the menu changes
+           /*A test that ensures the menu changes
            * visibility when the menu icon is clicked. This test
-           * should have two expectations: does the menu display when
-           * clicked and does it hide when clicked again.
+           * should have two expectations: does the menu icon display 
+           * when clicked and does it hide when clicked again.
            */          
-          it("toggles on: menu display when clicked and toggles off: menu hide when clicked again is working", function(){
+          it("toggles on: menu display when clicked and toggles off: menu hide when clicked again is working", () => {
             //define variables for DOM elements needed
-            const body = document.querySelector('body');
             const menu = document.querySelector(".menu-icon-link");
             //call the click() function on the element with the menu-icon-link
             menu.click();
@@ -100,8 +101,8 @@ $(function() {
     });
 
     // a new test suite named "Initial Entries" 
-    describe("Initial Entries", function(){
-         /* a test that ensures when the loadFeed
+    describe("Initial Entries", () => {
+         /*A test that ensures when the loadFeed
          * function is called and completes its work, there is at least
          * a single .entry element within the .feed container.
          * Remember, loadFeed() is asynchronous so this test will require
@@ -109,59 +110,65 @@ $(function() {
          */         
          //before the tests run check that loadFeed has been called so i call loadFeed(0) once via index 0 inserted 
          beforeEach(function(done){
-            //done gets used in our callback cb function in the loadFeed function in app.js
+            //done gets used in our callback cb function in the loadFeed function in app.js 
+            //meaning we can pass a function that let’s our Jasmine test know that our before each function has “finished” and proceed with our test
             loadFeed(0, done);
          });
 
-         it('We see 1 or more .entry within .feed after loadFeed() runs. loadFeed is called + completes work successfully asynchronous', function(){
+         it('We see 1 or more .entry within .feed after loadFeed() runs. loadFeed is called + completes work successfully asynchronously', () => {
             const feed = document.querySelector('.feed');
-            expect(feed.children.length > 0).toBe(true);
-            //could have wrote instead of above these lines as a test:
-            //const feedEnriesLength = document.querySelector(".feed").getElementByClassName("entry").length; 
-            //epect(feedEntriesLength).toBeGreaterThan(0); don();
+            //expect(feed.children.length > 0).toBe(true); if i used this line to test it could later fail by .feed getting populated by an element that has a dif class to .entry 
+            //so instead i wrote my test to specifically look for the elemet with .entry class inside of .feed
+            //best practice to be as specific as posible in test target exactly what you seek at its dom level
+            //query .feed and get its element with class name of .entry
+            const feedEntriesLength = document.querySelector(".feed").getElementsByClassName("entry").length; 
+            //expect  an element to have a class of at least one .entry 
+            expect(feedEntriesLength).toBeGreaterThan(0); 
 
          });
 
     });
 
-    /* a new test suite named "New Feed Selection" */
-    describe("New Feed Selection", function(){
-            //we need to use feed in both functions we’ll define it here the outer function scope:
+     /* A test that ensures when a new feed is loaded
+      * by the loadFeed function that the content actually changes.
+      * Remember, loadFeed() is asynchronous. */    
+    describe("New Feed Selection", () => {        
+
+            //we need to use feed in both functions so I define it here the outer function scope all inner functions have open access to it now
             const feed = document.querySelector('.feed');
             //store the first feed’s content in a new empty array variable called firstFeed:
-            const firstFeedEntry  = [];
+            //define variable of expected irst feed content = assign it a value below
+            let firstFeedContent;
 
-        /* a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.*/
-        beforeEach(function(done){
+        beforeEach( (done) => {
             //difference between this test and the one before, is we need to load 2 different feeds and check that the feed content changes
-            //done() can only be called once, we call it in the final async function to let Jasmine know when to continue
-            loadFeed(0);            
-            //console.log(feed.children[0].innerText); used this to test things are working earlier
-            Array.from(feed.children).forEach(function(entry){
-                firstFeedEntry.push(entry.innerText);
-            });
+            //done() can only be called once, we call it in the final async loadFeed() function below to let Jasmine know when to continue
+          loadFeed(0, () =>{
 
-            loadFeed(1, done);
-         });
+                        let firstFeedContent = document.querySelector('.feed').innerHTML;
 
-          it("feed content changes when the feed is loaded", function(){
-            //console.log(feed.children[0].innerText); tested from earlier
-            //convert the feed children into an array and loop over each “entry”:
-            Array.from(feed.children).forEach(function(entry, index){
-            //cosnole.log(entry.innerText, firstFeed[index], entry.innerText === firstFeed[index]);
-            expect(entry.innerText === firstFeedEntry[index]).not.toBe(true);             
-                });
+                loadFeed(1, () => {
+                     //done() can only be called once, we call it in the final async loadFeed() function below to let Jasmine know when to continue
+                    done();
+                });                        
+          });
+        }); 
+        //here we check if the content inside the first loadFeed is different from the content in side the second loadFeed call
+        it("feed content changes when two different loadFeed() are called", ((done) => {
+                //i called the async loadFeed a second time and after it is done i store its inner HTML in a variable to compare later
+                let newFeedContent = document.querySelector(".feed").innerHTML;
+                expect(firstFeedContent).not.toBe(newFeedContent);
+                 //done() can only be called once, we call it in the final async loadFeed() function below to let Jasmine know when to continue
+                done();
+        }));
 
-         });
-
-    });
+    }); 
 
 }());
 
-
+// Project 5 Feed Reader Testing with Jasmine
 // Code reused from the following sources:
 // The starter code for this project comes from the following Udacity GitHub address of https://github.com/udacity/frontend-nanodegree-feedreader. I forked it to my git repo and cloned it.
 // Some code Mohamed Riaad study jam at https://www.youtube.com/watch?v=Ut_L8YUImbw&index=6&list=PLpRo6-g-F-AOUsTQbKY1EYNpqXtxCT4a-&t=27s  
 // Some code from the walkthrough tutorials by Matthew Cranford at https://matthewcranford.com.
+// Some code from Daniela Kuester project https://github.com/DanielaKuester helped me understnad async with arrow functions 
